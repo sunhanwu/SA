6998a8f5fbd18249ca0253fb5e7cd1592396c175
sunhanwu-imac <1965190613@qq.com>
2021-04-24 23:43:57 +0800

    add func precessingTrain

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -19,6 +19,33 @@ def pcap2png(pcap_path:str, png_path:str, png_size=28):
     img = Image.fromarray(img_array)
     img.save(png_path)
 
+def precessingTrain(trainDataPath, T='a', length=784, top_num=60000):
+    """
+    预处理训练数据
+    trainDataPath: 训练数据位置
+    T: 预处理的格式，a表示保留所有层，7表示只保留应用层
+    """
+    assert T in ['a', '7']
+    if T == 'a':
+        session_type = 'AllLayers'
+    else:
+        session_type = 'L7'
+    for week in os.listdir(trainDataPath):
+        week_path = os.path.join(trainDataPath, week)
+        for day in os.listdir(week_path):
+            day_path = os.path.join(week_path, day)
+            flow_path = os.path.join(day_path, 'flow')
+            format_data_path = os.path.join(day_path, length)
+            tcpdmp = "{}/tcpdump".format(day_path)
+            label_file = "{}/tcpdump.list".format(day_path)
+            pcap2session_cmd = "bash pcap2session.sh {} {} -f".format(tcpdmp, os.path.join(day_path, flow_path))
+            print(pcap2session_cmd)
+            processSeeion_cmd = "bash processSeeion.sh {}/{}/ {}/ {} {}".format(flow_path, session_type, format_data_path, top_num, length)
+            print(processSeeion_cmd)
+
+
+
 
 if __name__ == '__main__':
-    pcap2png('./test.bin', './test.png')
+    # pcap2png('./test.bin', './test.png')
+    precessingTrain()
