808677ef19a215a8e8ca2dafbc3a6fbc04b85ee5
sunhanwu-imac <1965190613@qq.com>
2021-04-25 00:03:41 +0800

    fix bugs in precessingTrain

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -19,7 +19,7 @@ def pcap2png(pcap_path:str, png_path:str, png_size=28):
     img = Image.fromarray(img_array)
     img.save(png_path)
 
-def precessingTrain(trainDataPath, T='a', length=784, top_num=60000):
+def precessingTrain(trainDataPath, T='a', length=784, top_num=1000):
     """
     预处理训练数据
     trainDataPath: 训练数据位置
@@ -35,17 +35,17 @@ def precessingTrain(trainDataPath, T='a', length=784, top_num=60000):
         for day in os.listdir(week_path):
             day_path = os.path.join(week_path, day)
             flow_path = os.path.join(day_path, 'flow')
-            format_data_path = os.path.join(day_path, length)
+            format_data_path = os.path.join(day_path, str(length))
             tcpdmp = "{}/tcpdump".format(day_path)
             label_file = "{}/tcpdump.list".format(day_path)
-            pcap2session_cmd = "bash pcap2session.sh {} {} -f".format(tcpdmp, os.path.join(day_path, flow_path))
-            print(pcap2session_cmd)
+            pcap2session_cmd = "bash pcap2session.sh {} {} -f".format(tcpdmp, flow_path)
+            os.system(pcap2session_cmd)
             processSeeion_cmd = "bash processSeeion.sh {}/{}/ {}/ {} {}".format(flow_path, session_type, format_data_path, top_num, length)
-            print(processSeeion_cmd)
+            os.system(processSeeion_cmd)
 
 
 
 
 if __name__ == '__main__':
     # pcap2png('./test.bin', './test.png')
-    precessingTrain()
+    precessingTrain("../../data/train")
